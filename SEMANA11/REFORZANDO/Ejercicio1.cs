fee85c6cac6104ff07be19c58470ae27a364cc03
Sofia <SofiaCandiottiS@gmail.com>
2025-11-01 10:44:06 -0500

    Subiendo ejercicio 1

---
+++ SEMANA11/REFORZANDO/Ejercicio1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace REFORZANDO
+{
+    internal class Ejercicio1
+    {
+        static void Main(string[] args)
+        {
+            double[] PrecioProducto = new double[5] {12.50, 15.60, 20.40, 13.80, 16.0 };
+            int[] Cantidad = {20,40,35,30,25 };
+
+            double[] Multiplicar = { };
+
+            Console.Write("La lista de los precios de los productos: ");
+
+        }
+    }
+}
